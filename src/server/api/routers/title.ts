e5a7306f523b8897c8fe541336fb0401c13f6834
gayashan4lk <gayashan4lk@gmail.com>
2023-11-08 16:50:17 +0530

    Update OpenAI API key in title router

--- src/server/api/routers/title.ts
+++ src/server/api/routers/title.ts
@@ -7,18 +7,20 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 console.log(env.OPENAI_API_KEY);
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: env.OPENAI_API_KEY,
 });
 
 export const titleRouter = createTRPCRouter({
   getAiTitle: publicProcedure.query(async () => {
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "You are a helpful assistant." }],
-      model: "gpt-3.5-turbo",
-    });
-
-    console.log("completion.choices:", completion.choices[0]);
-
-    return completion.choices[0];
+    try {
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: "You are a helpful assistant." }],
+        model: "gpt-3.5-turbo",
+      });
+      //console.log("completion.choices:", completion.choices[0]);
+      return completion.choices[0];
+    } catch (error) {
+      console.error("error:", error);
+    }
   }),
 });
